a2e845ec94e24ccee9b1daba5be3af1b4c8ac0d0
Dmytro Melnychenko <31704436+kotba3ilio@users.noreply.github.com>
2024-10-30 22:56:19 +0200

    homework-4

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,21 @@
-function getElementWidth(content, padding, border) {
-  var contentNumber = Number.parseFloat(content.replace('px', ''));
-  var paddingNumber = Number.parseFloat(padding.replace('px', ''));
-  var borderNumber = Number.parseFloat(border.replace('px', ''));
-  return (paddingNumber * 2) + (borderNumber * 2) + contentNumber;
-}
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  }
+};
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
